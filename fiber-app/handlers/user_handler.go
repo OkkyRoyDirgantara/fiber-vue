133844a53043934393af572d6353745e5bf2b9f8
OkkyRoyDirgantara <okkyroydirgantara@gmail.com>
2023-12-09 11:58:12 +0700

    feat: create user

---
+++ fiber-app/handlers/user_handler.go
@@ -0,0 +1,44 @@
+package handlers
+
+import (
+	"fiber-vue/database"
+	"fiber-vue/utils"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type User struct {
+	Name     string `validate:"required,min=5,max=20"`
+	Email    string `validate:"required,email"`
+	Password string `validate:"required,min=8"`
+}
+
+func UserRegister(ctx *fiber.Ctx) error {
+	// Parse request body into NewUser struct
+	User := new(User)
+	if err := ctx.BodyParser(User); err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "error", "message": "Error on register request", "data": err})
+	}
+
+	// Validate the NewUser struct
+	if err := utils.ValidationInput(User); err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "error", "message": "Validation error", "data": err.Error()})
+	}
+
+	// Hash the password
+	hash, err := utils.HashPassword(User.Password)
+	if err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "error", "message": "Error on register request", "data": err})
+	}
+
+	// Update the password with the hashed one
+	User.Password = hash
+
+	// Save the user to the database
+	db := database.DBConn
+	if err := db.Create(&User).Error; err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "error", "message": "Error on register request", "data": err})
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{"status": "success", "message": "User successfully registered", "data": User})
+}
