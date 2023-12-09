fe29dbbbf5dd9ee6ffea7a754892c0da58771f7f
OkkyRoyDirgantara <okkyroydirgantara@gmail.com>
2023-12-09 13:54:55 +0700

    feat: add feature create product

--- vue-project/src/views/ProductsView.vue
+++ vue-project/src/views/ProductsView.vue
@@ -1,5 +1,41 @@
 <template>
   <div class="container mt-5">
+    <h2 class="mt-5">Create Product</h2>
+    <form @submit.prevent="createProduct" class="mt-3">
+      <div class="mb-3">
+        <label for="name" class="form-label">Product Name:</label>
+        <input v-model="createData.name" type="text" class="form-control" required />
+      </div>
+
+      <div class="mb-3">
+        <label for="code" class="form-label">Product Code:</label>
+        <input v-model="createData.code" type="text" class="form-control" required />
+      </div>
+
+      <div class="mb-3">
+        <label for="stock" class="form-label">Product Stock:</label>
+        <input v-model="createData.stock" type="number" class="form-control" min="1" required />
+      </div>
+
+      <div class="mb-3">
+        <label for="description" class="form-label">Product Description:</label>
+        <textarea v-model="createData.description" class="form-control" rows="3" required></textarea>
+      </div>
+
+      <div class="mb-3">
+        <label for="status" class="form-label">Product Status:</label>
+        <select v-model="createData.status" class="form-select" required>
+          <option value=true>Active</option>
+          <option value=false>Inactive</option>
+        </select>
+      </div>
+
+      <button type="submit" class="btn btn-primary">Create Product</button>
+    </form>
+    <div v-if="errorMessageCreate" class="alert alert-danger mt-3" role="alert">
+      {{ errorMessageCreate }}
+    </div>
+
     <h2 class="mt-5">Update Product Quantity</h2>
     <form @submit.prevent="updateQuantity" class="mt-3">
       <div class="mb-3">
@@ -53,7 +89,15 @@ export default {
         action: 'add',
         amount: 1
       },
-      errorMessage: null
+      createData: {
+        name: '',
+        code: '',
+        stock: 1,
+        description: '',
+        status: true,
+      },
+      errorMessage: null,
+      errorMessageCreate: null
     }
   },
   mounted() {
@@ -80,11 +124,37 @@ export default {
           }
         })
         this.fetchProducts()
+        this.updateData = {
+          code: '',
+          action: 'add',
+          amount: 1
+        }
         this.errorMessage = null
       } catch (error) {
         console.error('Error updating quantity:', error)
         this.errorMessage = 'Error updating quantity: ' + error.response.data.message
       }
+    },
+    async createProduct() {
+      try {
+        await axios.post('http://127.0.0.1:3000/api/v1/product', this.createData, {
+          headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('token')
+          }
+        })
+        this.fetchProducts()
+        this.createData = {
+          name: '',
+          code: '',
+          stock: 1,
+          description: '',
+          status: true,
+        }
+        this.errorMessageCreate = null
+        }catch (error) {
+          console.error('Error creating product:', error)
+          this.errorMessageCreate = 'Error creating product: ' + error.response.data.message
+        }
     }
   }
 }
